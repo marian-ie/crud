d697a7d3be07dd56d146bb44478d4d8043a384d8
marian-ie <mariantalaid@gmail.com>
2025-04-29 19:22:49 +0800

    Add files via upload

---
+++ task3/test_connection.php
@@ -0,0 +1,17 @@
+<?php
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+
+    include 'database.php';
+    include 'DbTest.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $test = new DbTest($db);
+
+    $response = $test->checkConnection();
+
+    echo json_encode($response);
+
+?>
